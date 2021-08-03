9662f60351d2202b59383e182d1b39817e89fbc7
Krassowsky <krassowsky@MacBook-Pro-Krassowsky.local>
2021-08-03 20:16:20 +0800

    Docker Compose Env Vars added

--- frontend/src/services/store.ts
+++ frontend/src/services/store.ts
@@ -6,7 +6,7 @@ import { createStore, applyMiddleware, Store } from 'redux'
 import thunk from 'redux-thunk'
 
 export const request = axios.create({
-  baseURL: 'http://localhost/api',
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
 })
 
